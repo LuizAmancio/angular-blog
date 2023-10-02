931b7753bc355307fcba09e00bc15900cc19cc2c
LuizAmancioCeub <luiz.amancio@sempreceub.com>
2023-10-02 15:53:02 -0300

    terminado

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title":"Santos vira para cima do Bahia e vence confronto direto em plena Fonte Nova",
+        "description":" Após três derrotas consecutivas, o Peixe voltou a vencer na reestreia do técnico interino Marcelo Fernandes. E não qualquer triunfo. Num confronto direto contra o Bahia, na noite desta segunda-feira (18), em plena Arena Fonte Nova, o Peixe conseguiu uma virada do alívio para cima do Esquadrão: 2 a 1.",
+        "img":"https://medias.itatiaia.com.br/generic/gol-da-vitoria-do-santos-veio-aos-48-minutos-do-segundo-tempo-com-julio-furch-1f1db540-80c3-4f80-86bf-cd5a6de36cbe.medium.webp"
+    },
+    {
+        "id": "2",
+        "title":"Fluminense bate o Cruzeiro e entra no G4 do Brasileirão",
+        "description":"Mesmo com a cabeça na Libertadores, Fluminense conseguiu dominar as ações do jogo e contou com cobrança de falta venenosa de Léo Fernández para vencer o Cruzeiro",
+        "img":"https://trivela.com.br/wp-content/uploads/2023/09/Leo-Fernandez-e-John-Kennedy-comemoram-gol-que-deu-vitoria-ao-Fluminense-sobre-o-Cruzeiro-e1695263504300-1536x862.jpg"
+    },
+    {
+        "id": "3",
+        "title":"Fluminense bate o Cruzeiro e entra no G4 do Brasileirão",
+        "description":"Mesmo com a cabeça na Libertadores, Fluminense conseguiu dominar as ações do jogo e contou com cobrança de falta venenosa de Léo Fernández para vencer o Cruzeiro",
+        "img":"https://www.sites-de-apostas.net/wp-content/uploads/2023/09/ezgif.com-gif-maker-100-768x631.jpg"
+    },
+    {
+        "id": "4",
+        "title":"Fluminense bate o Cruzeiro e entra no G4 do Brasileirão",
+        "description":"Mesmo com a cabeça na Libertadores, Fluminense conseguiu dominar as ações do jogo e contou com cobrança de falta venenosa de Léo Fernández para vencer o Cruzeiro",
+        "img":"https://trivela.com.br/wp-content/uploads/2023/09/Leo-Fernandez-e-John-Kennedy-comemoram-gol-que-deu-vitoria-ao-Fluminense-sobre-o-Cruzeiro-e1695263504300-1536x862.jpg"
+    }
+]
